d8c356cc7bbd4604d3158356f060f51d2cd5e3dc
Melek-Ustun <melek.ustun@etu.u-pec.fr>
2026-01-09 14:57:00 +0100

    gitignore + class Shape

---
+++ Shape.cpp
@@ -0,0 +1,9 @@
+// Shape.cpp
+#include "Shape.h"
+
+// Initialisation du membre statique
+int Shape::nextId = 1;
+
+Shape::Shape() : id(nextId++) {
+    // L'ID est automatiquement incrémenté
+}
---
+++ Shape.h
@@ -0,0 +1,27 @@
+// Shape.h
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <iostream>
+#include <vector>
+#include "Point.h"
+// Déclaration anticipée
+class Pixel;
+class Shape {
+    protected:
+        int id;
+        static int nextId;
+
+    public:
+        // Constructeur
+        Shape();
+
+        // Destructeur virtuel
+        virtual ~Shape() = default;
+        // Méthodes virtuelles pures (classe abstraite)
+        virtual void print() const = 0;
+        virtual std::vector<Pixel> toPixels() const = 0;
+        // Getter
+        int getId() const { return id; }
+};
+
+#endif // SHAPE_H
